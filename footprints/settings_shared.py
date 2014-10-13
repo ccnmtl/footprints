e9187bb02caa6648674d0ddae4d1e4c0f8fa447c
Susan Dreher <sdreher@columbia.edu>
2014-10-13 14:28:03 -0400

    adding haystack.

--- footprints/settings_shared.py
+++ footprints/settings_shared.py
@@ -76,7 +76,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
     'django_statsd.middleware.GraphiteRequestTimingMiddleware',
     'django_statsd.middleware.GraphiteMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,7 +88,7 @@ MIDDLEWARE_CLASSES = (
     'impersonate.middleware.ImpersonateMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'waffle.middleware.WaffleMiddleware',
-)
+]
 
 ROOT_URLCONF = 'footprints.urls'
 
