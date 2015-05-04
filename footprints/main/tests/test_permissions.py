6ff5c0b4945afcb4d68c3772ac3a829494825e89
Susan Dreher <sdreher@columbia.edu>
2015-05-04 14:23:35 -0400

    Open site for public viewing * Browse/Search/View are available without authentication. * All authenticated users can edit * Tweaking navbars to reflect standard site elements.

--- footprints/main/tests/test_permissions.py
+++ footprints/main/tests/test_permissions.py
@@ -1,51 +1,21 @@
+from django.contrib.auth.models import AnonymousUser
 from django.test.client import RequestFactory
 from django.test.testcases import TestCase
 
-from footprints.main.permissions import IsOwnerOrReadOnly, IsStaffOrReadOnly
+from footprints.main.permissions import IsLoggedInOrReadOnly
 from footprints.main.tests.factories import FootprintFactory, UserFactory
 
 
-class IsOwnerOrReadOnlyTest(TestCase):
+class IsLoggedInOrReadOnlyTest(TestCase):
 
     def setUp(self):
-        self.permission = IsOwnerOrReadOnly()
+        self.permission = IsLoggedInOrReadOnly()
         self.get = RequestFactory().get('/', {})
         self.post = RequestFactory().post('/', {})
 
-    def test_has_object_permission(self):
-        user = UserFactory()
-        user2 = UserFactory()
-        staff = UserFactory(is_staff=True)
-        obj = FootprintFactory(created_by=user)
-
-        self.get.user = user2
-        auth = self.permission.has_object_permission(self.get, None, obj)
-        self.assertTrue(auth)
-
-        self.post.user = user2
-        auth = self.permission.has_object_permission(self.post, None, obj)
-        self.assertFalse(auth)
-
-        self.post.user = user
-        auth = self.permission.has_object_permission(self.post, None, obj)
-        self.assertTrue(auth)
-
-        self.post.user = staff
-        auth = self.permission.has_object_permission(self.post, None, obj)
-        self.assertTrue(auth)
-
-
-class IsStaffOrReadOnlyTest(TestCase):
-
-    def setUp(self):
-        self.permission = IsStaffOrReadOnly()
-        self.get = RequestFactory().get('/', {})
-        self.post = RequestFactory().post('/', {})
-
-    def test_has_object_permission(self):
-        user = UserFactory()
-        staff = UserFactory(is_staff=True)
-        obj = FootprintFactory(created_by=user)
+    def test_not_logged_in(self):
+        user = AnonymousUser()
+        obj = FootprintFactory()
 
         self.get.user = user
         auth = self.permission.has_object_permission(self.get, None, obj)
@@ -55,6 +25,14 @@ class IsStaffOrReadOnlyTest(TestCase):
         auth = self.permission.has_object_permission(self.post, None, obj)
         self.assertFalse(auth)
 
-        self.post.user = staff
+    def test_logged_in(self):
+        user = UserFactory()
+        obj = FootprintFactory()
+
+        self.get.user = user
+        auth = self.permission.has_object_permission(self.get, None, obj)
+        self.assertTrue(auth)
+
+        self.post.user = user
         auth = self.permission.has_object_permission(self.post, None, obj)
         self.assertTrue(auth)
