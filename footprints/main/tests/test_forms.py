6f3115daea16a8739f2ffebdc7712f1385d34f40
Susan Dreher <sdreher@columbia.edu>
2015-05-06 10:12:22 -0400

    SearchForm - a little test coverage.

--- footprints/main/tests/test_forms.py
+++ footprints/main/tests/test_forms.py
@@ -1,9 +1,7 @@
 from django import forms
 from django.test.testcases import TestCase
-from haystack.query import EmptySearchQuerySet
 
 from footprints.main.forms import FootprintSearchForm
-from footprints.main.tests.factories import FootprintFactory
 
 
 class SearchFormTest(TestCase):
@@ -20,32 +18,7 @@ class SearchFormTest(TestCase):
         self.assertEquals(form.fields['q'].widget.attrs['placeholder'],
                           'Titles, People, Places')
 
-    def test_search(self):
-        footprint1 = FootprintFactory(title='Alpha')
-        footprint2 = FootprintFactory(title='Beta')
-        footprint3 = FootprintFactory(title='Gamma')
-
+    def test_empty_search(self):
         form = FootprintSearchForm()
         sqs = form.search()
         self.assertEquals(sqs.count(), 0)
-
-        form.is_bound = True
-        sqs = form.search()
-        # self.assertEquals(sqs.count(), 3)
-
-#     def search(self):
-#         if not self.cleaned_data.get('q'):
-#             sqs = self.searchqueryset.all()
-#         else:
-#             sqs = self.searchqueryset.auto_query(self.cleaned_data['q'])
-# 
-#         sqs = sqs.exclude(django_ct__in=["main.imprint",
-#                                          "main.place",
-#                                          "main.person"])
-# 
-#         if self.load_all:
-#             sqs = sqs.load_all()
-# 
-#         sqs = sqs.models(*self.get_models())
-#         sqs = sqs.order_by('sort_by')
-#         return sqs
