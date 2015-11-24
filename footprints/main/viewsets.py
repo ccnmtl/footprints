fd8546edd5bff08ad03491f8acdd1b8678309c94
Susan Dreher <sdreher@columbia.edu>
2015-11-24 15:19:58 -0500

    Use django-restframework IsAuthenticatedOrReadOnly

--- footprints/main/viewsets.py
+++ footprints/main/viewsets.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from footprints.main.models import (
     Footprint, Actor, Person, Role, WrittenWork, Language, ExtendedDateFormat,
     Place, Imprint, BookCopy, StandardizedIdentification, DigitalFormat,
     DigitalObject, StandardizedIdentificationType)
-from footprints.main.permissions import IsLoggedInOrReadOnly
 from footprints.main.serializers import (
     FootprintSerializer, LanguageSerializer, RoleSerializer,
     ExtendedDateFormatSerializer, ActorSerializer, PersonSerializer,
@@ -18,49 +18,49 @@ from footprints.main.serializers import (
 class FootprintViewSet(viewsets.ModelViewSet):
     queryset = Footprint.objects.all()
     serializer_class = FootprintSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class ExtendedDateFormatViewSet(viewsets.ModelViewSet):
     queryset = ExtendedDateFormat.objects.all()
     serializer_class = ExtendedDateFormatSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class WrittenWorkViewSet(viewsets.ModelViewSet):
     queryset = WrittenWork.objects.all()
     serializer_class = WrittenWorkSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
         """
@@ -76,13 +76,13 @@ class WrittenWorkViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class ImprintViewSet(viewsets.ModelViewSet):
     model = Imprint
     serializer_class = ImprintSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
         qs = Imprint.objects.all()
@@ -101,7 +101,7 @@ class ImprintViewSet(viewsets.ModelViewSet):
 class BookCopyViewSet(viewsets.ModelViewSet):
     model = BookCopy
     serializer_class = BookCopySerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
         qs = BookCopy.objects.all()
@@ -116,22 +116,22 @@ class BookCopyViewSet(viewsets.ModelViewSet):
 class StandardizedIdentificationViewSet(viewsets.ModelViewSet):
     queryset = StandardizedIdentification.objects.all()
     serializer_class = StandardizedIdentificationSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class StandardizedIdentificationTypeViewSet(viewsets.ModelViewSet):
     queryset = StandardizedIdentificationType.objects.all()
     serializer_class = StandardizedIdentificationTypeSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class DigitalFormatViewSet(viewsets.ModelViewSet):
     queryset = DigitalFormat.objects.all()
     serializer_class = DigitalFormatSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class DigitalObjectViewSet(viewsets.ModelViewSet):
     queryset = DigitalObject.objects.all()
     serializer_class = DigitalObjectSerializer
-    permission_classes = (IsLoggedInOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
