28e18c24eb4d4f702f3a5bb4eaf0cd8f84330995
Anders Pearson <anders@columbia.edu>
2015-12-07 12:13:48 +0000

    convert to ccnmtlsettings

--- footprints/settings_shared.py
+++ footprints/settings_shared.py
@@ -1,25 +1,12 @@
 # Django settings for footprints project.
 import os.path
 import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+project = 'footprints'
+base = os.path.dirname(__file__)
 
-ADMINS = ()
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'footprints',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
+locals().update(common(project=project, base=base))
 
 HAYSTACK_CONNECTIONS = {
     'default': {
@@ -34,90 +21,29 @@ HAYSTACK_CONNECTIONS = {
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 15
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
 PROJECT_APPS = [
     'footprints.main',
 ]
 
-ALLOWED_HOSTS = ['localhost', '.ccnmtl.columbia.edu']
-
 USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/footprints/uploads/"
-MEDIA_URL = '/uploads/'
-STATIC_URL = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'stagingcontext.staging_processor',
-    'gacontext.ga_processor',
-    'djangowind.context.context_processor',
-    'django.template.context_processors.static',
+TEMPLATE_CONTEXT_PROCESSORS += [  # noqa
     'django.template.context_processors.csrf',
-)
+]
 
-MIDDLEWARE_CLASSES = [
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
+MIDDLEWARE_CLASSES += [  # noqa
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'waffle.middleware.WaffleMiddleware',
     'audit_log.middleware.UserLoggingMiddleware',
     'reversion.middleware.RevisionMiddleware'
 ]
 
-ROOT_URLCONF = 'footprints.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/footprints/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django_markwhat',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
+INSTALLED_APPS += [  # noqa
     'sorl.thumbnail',
-    'django.contrib.admin',
     'tagging',
     'typogrify',
-    'compressor',
-    'django_statsd',
     'bootstrapform',
-    'debug_toolbar',
-    'waffle',
-    'django_jenkins',
-    'smoketest',
     'infranil',
     'django_extensions',
-    'impersonate',
-    'gunicorn',
     'haystack',
     'footprints.main',
     'geoposition',
@@ -125,69 +51,10 @@ INSTALLED_APPS = [
     'reversion'
 ]
 
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'footprints'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[footprints] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "footprints@ccnmtl.columbia.edu"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
 CONTACT_US_EMAIL = 'footprints@columbia.edu'
 
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../media")
-STATICFILES_DIRS = (
-)
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-
-# WIND settings
-
-AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
-                           'django.contrib.auth.backends.ModelBackend', )
-CAS_BASE = "https://cas.columbia.edu/"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = [
-    'anp8', 'jb2410', 'zm4', 'cld2156',
-    'sld2131', 'amm8', 'mar227', 'lrw2128', 'njn2118']
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = '/'
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
@@ -202,17 +69,6 @@ REST_FRAMEWORK = {
 }
 
 if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
     HAYSTACK_CONNECTIONS = {
         'default': {
             'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
