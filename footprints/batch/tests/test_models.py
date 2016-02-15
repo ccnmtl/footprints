7e7c7ecb4b7da775ac06eb0fc2c7907294ccebb1
Susan Dreher <sdreher@columbia.edu>
2016-02-15 10:34:19 -0500

    Batch Import: improve criteria for similar footprints

--- footprints/batch/tests/test_models.py
+++ footprints/batch/tests/test_models.py
@@ -2,7 +2,8 @@ from django.test.testcases import TestCase
 
 from footprints.batch.tests.factories import BatchRowFactory
 from footprints.main.tests.factories import ImprintFactory, BookCopyFactory, \
-    FootprintFactory, WrittenWorkFactory
+    FootprintFactory, WrittenWorkFactory, StandardizedIdentificationFactory, \
+    ActorFactory
 
 
 class BatchRowTest(TestCase):
@@ -17,15 +18,31 @@ class BatchRowTest(TestCase):
         self.assertEquals(row.aggregate_notes(), 'Levita, Elijah.')
 
     def test_similar_footprints(self):
-        row = BatchRowFactory()
+        bhb_number = '1234'
+        actor = ActorFactory()
+        work = WrittenWorkFactory()
+        imprint = ImprintFactory(work=work)
+        si = StandardizedIdentificationFactory(identifier=bhb_number)
+        imprint.standardized_identifier.add(si)
+
+        copy = BookCopyFactory(imprint=imprint)
+
+        row = BatchRowFactory(
+            imprint_title=imprint.title,
+            publisher=imprint.actor.first().person.name,
+            publication_location='50.064650,19.944979',
+            bhb_number=bhb_number,
+            writtenwork_title=work.title,
+            writtenwork_author=work.actor.first().person.name,
+            footprint_actor=actor.person.name,
+            footprint_location='50.064650,19.944979')
+
         self.assertFalse(row.similar_footprints().exists())
 
-        work = WrittenWorkFactory(title=row.writtenwork_title)
-        imprint = ImprintFactory(title=row.imprint_title, work=work)
-        copy = BookCopyFactory(imprint=imprint)
-        FootprintFactory(
+        fp = FootprintFactory(
             medium=row.medium, provenance=row.provenance,
             call_number=row.call_number, notes=row.aggregate_notes(),
             book_copy=copy)
+        fp.actor.add(actor)
 
         self.assertTrue(row.similar_footprints().exists())
