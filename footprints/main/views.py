fc2ed2a09bc955d5eb078516526a041eb9416b16
Susan Dreher <sdreher@columbia.edu>
2017-05-16 16:48:23 -0400

    default value

--- footprints/main/views.py
+++ footprints/main/views.py
@@ -43,7 +43,8 @@ from footprints.mixins import (
 
 # returns important setting information for all web pages.
 def django_settings(request):
-    return {'settings': {'GOOGLE_MAP_API': settings.GOOGLE_MAP_API}}
+    return {'settings':
+            {'GOOGLE_MAP_API': getattr(settings, 'GOOGLE_MAP_API', '')}}
 
 
 class IndexView(TemplateView):
