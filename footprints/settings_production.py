42a4dfadad7a955370dd865eab2c84bc77490c15
Susan Dreher <sdreher@columbia.edu>
2018-03-13 14:19:39 -0400

    python3 - a few compatibility updates

--- footprints/settings_production.py
+++ footprints/settings_production.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from settings_shared import *
+from footprints.settings_shared import *
 from ccnmtlsettings.production import common
 
 locals().update(
--- footprints/settings_staging.py
+++ footprints/settings_staging.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from settings_shared import *
+from footprints.settings_shared import *
 from ccnmtlsettings.staging import common
 
 locals().update(
