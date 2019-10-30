de3362186293465fea682d356d6589a598ca477b
Susan Dreher <sdreher@gmail.com>
2019-10-30 14:37:52 -0400

    Layer list and form vues
    Splitting apart the "search" views into a list view and a form view.

--- media/js/app/pathmapper.js
+++ media/js/app/pathmapper.js
@@ -1,54 +1,29 @@
 requirejs(['./common'], function(common) {
-    const libs = ['jquery', 'Vue', 'mapVue', 'searchVue', 'utils'];
+    const libs = ['jquery', 'Vue', 'mapVue', 'layerListVue', 'utils'];
     requirejs(libs,
-        function($, Vue, map, search, utils) {
+        function($, Vue, map, layers, utils) {
             new Vue({
                 el: '#pathmapper-container',
                 data: function() {
                     return {
-                        criteria: {},
-                        total: null,
+                        layers: []
                     };
                 },
                 components: {
-                    'search-widget': search.SearchVue,
+                    'layer-list': layers.LayerListVue,
                     'google-map': map.GoogleMapVue
                 },
                 methods: {
-                    search: function() {
-                        if ($('html').hasClass('busy')) {
-                            return;
-                        }
-
-                        $('html').addClass('busy');
-                        this.searching = true;
-
-                        // retrieve book copies
-                        $.ajax({
-                            url: this.searchUrl,
-                            data: this.criteria,
-                            type: 'post'
-                        }).done((results) => {
-                            this.total = results.total;
-                            $('html').removeClass('busy');
-                            this.searching = false;
-                        });
-                    }
                 },
                 created: function() {
-                    this.searchUrl = Footprints.baseUrl + 'search/book/';
-
                     // Setup CSRF configuration and busy states
                     utils.ajaxSetup();
 
                     // eslint-disable-next-line
                     window.addEventListener('beforeunload', this.beforeUnload);
 
-                    this.$watch('criteria', this.search);
-                },
-                mounted: function() {
-                },
-                updated: function() {
+                    // @todo - initialize layers via template data
+                    // if accessing through a saved permalink
                 }
             });
         }
