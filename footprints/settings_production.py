d65a552b773e3a9bf9e413eef203678876cc1dd4
Nick Buonincontri <nbuonin@gmail.com>
2019-11-12 12:11:00 -0500

    Update flake8 syntax in settings files

--- footprints/settings_production.py
+++ footprints/settings_production.py
@@ -1,4 +1,4 @@
-from footprints.settings_shared import *  # noqa F403
+from footprints.settings_shared import *  # noqa: F403
 from ccnmtlsettings.production import common
 from django.conf import settings
 import sentry_sdk
@@ -6,10 +6,10 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,  # noqa F405
-        base=base,  # noqa F405
-        INSTALLED_APPS=INSTALLED_APPS,  # noqa F405
-        STATIC_ROOT=STATIC_ROOT  # noqa F405
+        project=project,  # noqa: F405
+        base=base,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
+        STATIC_ROOT=STATIC_ROOT  # noqa: F405
     ))
 
 
@@ -26,12 +26,12 @@ DATABASES = {
 
 
 try:
-    from footprints.local_settings import *  # noqa F403
+    from footprints.local_settings import *  # noqa: F403
 except ImportError:
     pass
 
 if hasattr(settings, 'SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=SENTRY_DSN,  # noqa F405
+        dsn=SENTRY_DSN,  # noqa: F405
         integrations=[DjangoIntegration()],
     )
--- footprints/settings_staging.py
+++ footprints/settings_staging.py
@@ -1,4 +1,4 @@
-from footprints.settings_shared import *  # noqa F403
+from footprints.settings_shared import *  # noqa: F403
 from ccnmtlsettings.staging import common
 from django.conf import settings
 import sentry_sdk
@@ -6,10 +6,10 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,  # noqa F405
-        base=base,  # noqa F405
-        STATIC_ROOT=STATIC_ROOT,  # noqa F405
-        INSTALLED_APPS=INSTALLED_APPS  # noqa F405
+        project=project,  # noqa: F405
+        base=base,  # noqa: F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS  # noqa: F405
     ))
 
 
@@ -26,13 +26,13 @@ DATABASES = {
 
 
 try:
-    from footprints.local_settings import *  # noqa F403
+    from footprints.local_settings import *  # noqa: F403
 except ImportError:
     pass
 
 if hasattr(settings, 'SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=SENTRY_DSN,  # noqa F405
+        dsn=SENTRY_DSN,  # noqa: F405
         integrations=[DjangoIntegration()],
         debug=True,
     )
