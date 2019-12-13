0166bf6ae390205c2f91a51efe745e27dd64dc20
Susan Dreher <sdreher@gmail.com>
2019-12-13 09:36:34 -0500

    Pathmapper: first steps to integrate dates into the form

--- footprints/pathmapper/views.py
+++ footprints/pathmapper/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime, date
+
 from django.views.generic.base import TemplateView, View
 
 from footprints.mixins import JSONResponseMixin
@@ -13,12 +15,30 @@ class MapView(TemplateView):
 
 class BookCopySearchView(JSONResponseMixin, View):
 
+    def min_year(self, sqs, key):
+        stats = sqs.stats(key).stats_results()
+        if not stats or not stats[key]['min']:
+            return date.min.year
+
+        return datetime.strptime(stats[key]['min'], '%Y-%m-%dT%H:%M:%SZ').year
+
+    def max_year(self, sqs, key):
+        stats = sqs.stats(key).stats_results()
+        if not stats or not stats[key]['min']:
+            return date.max.year
+        return datetime.strptime(stats[key]['max'], '%Y-%m-%dT%H:%M:%SZ').year
+
     def post(self, request):
         form = BookCopySearchForm(request.POST)
         if form.is_valid():
             sqs = form.search()
-
-            ctx = {'total': sqs.count()}
+            ctx = {
+                'total': sqs.count(),
+                'footprintMin': self.min_year(sqs, 'footprint_start_date'),
+                'footprintMax': self.max_year(sqs, 'footprint_end_date'),
+                'pubMin': self.min_year(sqs, 'pub_start_date'),
+                'pubMax': self.max_year(sqs, 'pub_end_date')
+            }
             return self.render_to_json_response(ctx)
 
         return self.render_to_json_response({'errors': form.errors})
