3448e7eed21326bd792004c64326703bc41fa384
Susan Dreher <sdreher@gmail.com>
2020-01-08 09:52:51 -0500

    settings_travis.py

--- footprints/settings_travis.py
+++ footprints/settings_travis.py
@@ -1,4 +1,13 @@
+# flake8: noqa
 import sys
+from footprints.settings_shared import *
+
+
+try:
+    from footprints.local_settings import *
+except ImportError:
+    pass
+
 
 if ('test' in sys.argv or 'jenkins' in sys.argv or 'validate' in sys.argv
         or 'check' in sys.argv):
