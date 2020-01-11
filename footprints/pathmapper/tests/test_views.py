9bf550930a27749701b3f20a6287a6436a4b7dc1
Susan Dreher <sdreher@gmail.com>
2020-01-10 19:54:29 -0500

    add test

--- footprints/pathmapper/tests/test_views.py
+++ footprints/pathmapper/tests/test_views.py
@@ -13,3 +13,17 @@ class BookCopySearchViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         the_json = loads(response.content.decode('utf-8'))
         self.assertEqual(the_json['total'], 0)
+
+
+class PathmapperTableViewTest(TestCase):
+
+    def test_post(self):
+        url = reverse('pathmapper-table-view')
+        response = self.client.post(url, {'layers': '{}'},
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        the_json = loads(response.content.decode('utf-8'))
+        self.assertFalse(the_json['page']['hasNext'])
+        self.assertFalse(the_json['page']['hasPrev'])
+        self.assertEqual(the_json['page']['number'], 1)
+        self.assertEqual(the_json['footprints'], [])
