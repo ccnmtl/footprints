a877ac937cb5328696eae25541c3406668ea49b5
Susan Dreher <sdreher@gmail.com>
2021-02-06 21:44:44 -0500

    Footprint Detail: Sort related footprints

--- footprints/main/templatetags/detailtags.py
+++ footprints/main/templatetags/detailtags.py
@@ -5,4 +5,7 @@ register = template.Library()
 
 @register.simple_tag
 def book_copy_footprints(fp):
-    return fp.book_copy.footprint_set.all().exclude(pk=fp.pk)
+    qs = fp.book_copy.footprint_set.all().exclude(pk=fp.pk)
+    lst = list(qs)
+    lst.sort(key=lambda obj: obj.sort_date())
+    return lst
