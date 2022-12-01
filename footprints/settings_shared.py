470d0780864b883ce22aacfbba779700e746ae31
Evan Petersen <emp2221@columbia.edu>
2022-12-01 11:14:02 -0500

    django-s3sign --> v0.3.2

--- footprints/settings_shared.py
+++ footprints/settings_shared.py
@@ -208,7 +208,7 @@ SPECTACULAR_SETTINGS = {
 GOOGLE_MAPS_REVERSE_GEOCODE = \
     'https://maps.googleapis.com/maps/api/geocode/json?address={},{}'
 
-AWS_STORAGE_BUCKET_NAME = "ccnmtl-footprints-static-dev"
+AWS_STORAGE_BUCKET_NAME = "ctl-footprints-static-dev"
 MEDIA_URL = 'https://%s.s3.amazonaws.com/uploads/' % AWS_STORAGE_BUCKET_NAME
 IMPERSONATE_REQUIRE_SUPERUSER = True
 
