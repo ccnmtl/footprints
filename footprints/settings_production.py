d5f9d1add0d0d692d5420a4dcecbacae04b3b8b7
Evan Petersen <emp2221@columbia.edu>
2026-07-29 16:19:49 -0400

    Amazon SQS configuration

--- footprints/settings_production.py
+++ footprints/settings_production.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from footprints.settings_shared import *  # noqa F403
+from footprints.settings_shared import (
+    project, base, STATIC_ROOT, INSTALLED_APPS, SENTRY_DSN, AWS_REGION)
 from ctlsettings.production import common, init_sentry
 
 
@@ -33,3 +34,9 @@ except ImportError:
 
 if hasattr(settings, 'SENTRY_DSN'):
     init_sentry(SENTRY_DSN)  # noqa F405
+
+if hasattr(settings, 'AWS_REGION'):
+    broker_transport_options = {
+        'region': AWS_REGION,
+        'queue_name_prefix': 'footprints-prod-'
+    }
